ea3af2c6fcb36d1d6b095406404959aa5e780854
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-22 22:25:16 -0500

    Yupdate

--- provider.tf
+++ provider.tf
@@ -8,10 +8,7 @@ terraform {
 }
 variable "do_token" {}
 
-variable "sshkey" {
-  description = "Public ssh key"
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCd8n54gAezdCHSwUWTMkNBut3WwT64Xtp+fQkfQ4IAMWoDBV8ScPXsO+DpkholMwNx6Xh9Aq07Y6jBr8n/bv8dak59GXht2IQW9qn5Ft6+3ie5/esMabdTX4/nzs2C5VMkOXL4wv6rSDXkYsjFzfhV0G9CN3YEf0DCvXHPADdhpQXIvUh9u3VfUT/I5xpaHHsSgvYoKyR5Q6iBFaMvbVmmvNkouvo+rGTph/HIEFOEC/Sra0dONMt6F+I2pViIkQ6+QDgQX6WpOUjgnWbLh+Rh2eY9Hb6fX85K+/EsqfWBvTpQg2W+w2iXM5M33gnGNR2gQifgurlvA+MPdfTES5NLthtNstAuPKHcnSxIZKRGAOKAQl5DWrU1P+JxATFCIqDpnzTnuPpONbBOEkzVQWSjKp2uvox9IrUQ/B8sgh1fhx3yBG4bAePqAoKrERL4qvMbs6TEhzvMQB47Pj9gMEcUb4HiPMtu3N8cSGK4TAiCKphroSmvBBezTMarsg5DMv0= daniel@windows10-client-0"
-}
+variable "pvt_key" {}
 provider "digitalocean" {
   token = var.do_token
 }
@@ -25,4 +22,19 @@ resource "digitalocean_droplet" "chefbot" {
     name    = "chef-bot"
     region  = "nyc2"
     size    = "s-1vcpu-1gb"
+    ssh_keys = [ var.ssh_key ]
+    connection {
+      host = self.ipv4_address
+      user = "root"
+      type = "ssh"
+      private_key = file(var.pvt_key)
+      timeout = "2m"
+    }
+    provisioner "remote-exec" {
+      inline = [
+        "export PATH=$PATH:/usr/bin",
+        "sudo apt update"
+      ]
+    }
+
 }
