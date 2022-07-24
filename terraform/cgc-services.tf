4bac9d126ff0ef70aa672238fa253e68a7e247ac
Dan Eager <81655709+DanEager19@users.noreply.github.com>
2022-07-23 19:10:46 -0500

    Finished tf script

--- terraform/cgc-services.tf
+++ terraform/cgc-services.tf
@@ -16,15 +16,23 @@ resource "digitalocean_droplet" "cgc-services" {
       "export PATH=$PATH:/usr/bin",
       "apt-get update && apt-get upgrade -y",
       "apt-get install git ufw -y",
-      "git clone https://github.com/DanEager19/cgc-service-devops",
-      "cd cgc-service-devops",
-      "./docker.sh",
-      "git clone https://github.com/DanEager19/kitchenware-request-api",
-      "git clone https://github.com/DanEager19/chef-bot",
-      "echo ${var.discord_api_token} > ./chef-bot/.env",
-      "echo ${var.guild_id} > ./chef-bot/.env",
-      "echo ${var.client_id} > ./chef-bot/.env",
-      "echo ${var.sendgrid_api_key} > ./kitchenware-requst-api/.env"
+      "git clone https://github.com/DanEager19/cgc-service-devops /root/cgc-service-devops",
+      "apt-get install ca-certificates gnupg lsb-release curl -y",
+      "mkdir -p /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "chmod a+r /etc/apt/keyrings/docker.gpg",
+      "apt-get update",
+      "apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y",
+      "git clone https://github.com/DanEager19/kitchenware-request-api  /root/cgc-service-devops/kitchenware-request-api",
+      "git clone https://github.com/DanEager19/chef-bot  /root/cgc-service-devops/chef-bot",
+      "touch /root/cgc-service-devops/kitchenware-request-api/.env",
+      "touch /root/cgc-service-devops/chef-bot/.env",
+      "echo TOKEN=\"${var.discord_api_token}\" >> /root/cgc-service-devops/chef-bot/.env",
+      "echo GUILD_ID=\"${var.guild_id}\" >> /root/cgc-service-devops/chef-bot/.env",
+      "echo CLIENT_ID\"${var.client_id}\" >> /root/cgc-service-devops/chef-bot/.env",
+      "echo SENDGRID_API_KEY=\"${var.sendgrid_api_key}\" >> /root/cgc-service-devops/kitchenware-request-api/.env",
+      "cd /root/cgc-service-devops && docker compose up --build -d"
     ]
   }
 }
